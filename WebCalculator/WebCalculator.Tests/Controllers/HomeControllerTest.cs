a37ff5316939becab6a56786de365438c77af047
eoneoff <eoneoff@gmail.com>
2019-01-17 17:06:10 +0200

    completed

--- WebCalculator/WebCalculator.Tests/Controllers/HomeControllerTest.cs
+++ WebCalculator/WebCalculator.Tests/Controllers/HomeControllerTest.cs
@@ -16,7 +16,7 @@ namespace WebCalculator.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void IndexReturnesView()
         {
             // Arrange
             var moq = new Mock<ICalculatorRepository.ICalculatorRepository>();
@@ -29,6 +29,67 @@ namespace WebCalculator.Tests.Controllers
 
             // Assert
             Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void IndexReturnesListOfStrings()
+        {
+            //Arrange
+            var moq = new Mock<ICalculatorRepository.ICalculatorRepository>();
+            moq.Setup(m => m.GetOperations(It.IsAny<string>())).Returns
+                (Task.FromResult(new List<string>() as IEnumerable<string>));
+            HomeController controller = new HomeController(moq.Object);
+
+            //Act
+            ViewResult result = controller.Index().Result as ViewResult;
+
+            //Assert
+            Assert.IsInstanceOfType(result.Model, typeof(IEnumerable<string>));
+        }
+
+        [TestMethod]
+        public void PostRecordReturnedJsonResult()
+        {
+            //Arrange
+            var moq = new Mock<ICalculatorRepository.ICalculatorRepository>();
+            HomeController controller = new HomeController(moq.Object);
+
+            //Act
+            JsonResult result = controller.PostRecord("").Result;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void PostRecortReturnsSuccess()
+        {
+            //Arrange
+            var moq = new Mock<ICalculatorRepository.ICalculatorRepository>();
+            HomeController controller = new HomeController(moq.Object);
+
+            //Act
+            JsonResult result = controller.PostRecord("").Result;
+
+            //Assert
+            Assert.AreEqual(result.Data.ToString(), "{\"Success\": \"true\"}");
+        }
+
+        [TestMethod]
+        public void PostRecordThrowsError()
+        {
+            //Arrange
+            var moq = new Mock<ICalculatorRepository.ICalculatorRepository>();
+            string exceptionMessage = "A \"Test eception\" had occured";
+            moq.Setup(m => m.SaveOperation(It.IsAny<string>(), It.IsAny<string>())).
+                Throws(new Exception(exceptionMessage));
+            HomeController controller = new HomeController(moq.Object);
+
+            //Act
+            JsonResult result = controller.PostRecord("").Result;
+
+            //Assert
+            Assert.AreEqual(result.Data.ToString(), $"{{\"Success\": \"false\", \"Error\":\"{exceptionMessage.Replace("\"", "'")}\"}}");
         }
     }
 }
